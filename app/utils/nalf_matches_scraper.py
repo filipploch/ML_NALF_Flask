289099432dc6cb9181bf586e215351531e0fdc49
filipploch <filip.ploch@gmail.com>
2024-01-22 22:51:19 +0100

    refactor nalf_matches_scraper.py and nalf_table_scraper.py files

--- app/utils/nalf_matches_scraper.py
+++ app/utils/nalf_matches_scraper.py
@@ -1,43 +1,66 @@
 from app.utils.scraper import Scraper
+from datetime import datetime
 
 
 class MatchesScraper(Scraper):
     def __init__(self, url):
         super().__init__(url)
 
-    def scrape_matches(self):
+    def scrape_matches(self, start_date_str, end_date_str):
         content = self.scrape_content()
         matches_list = content['table']
         division = content['title']
         data_objects_list = []
 
-        # Iteruj przez każdy wiersz
         for idx, match in enumerate(matches_list):
             data_object = {
                 "id": idx + 1,
-                "date": match.find_all('date')[0].text,
-                "teams": (match.find_all('a')[1].text).lstrip().split(' — '),
+                "date": match.find_all('date')[0].text[:-9],
+                "teams": match.find_all('a')[1].text.lstrip().split(' — '),
                 "result": self.get_match_result(match.find_all('a')[2].text),
             }
 
-            # Dodaj obiekt do listy
-            data_objects_list.append(data_object)
-
-        # Możesz zwrócić listę obiektów lub dalej przetwarzać dane
+            if self.is_date_between(data_object["date"], start_date_str, end_date_str):
+                data_objects_list.append(data_object)
         return {'matches': data_objects_list, 'division': division}
 
-    def get_match_result(self, _data):
+    @staticmethod
+    def get_match_result(_data):
         if ' - ' in _data:
             score_a, score_b = _data.split(' - ')
             return [int(score_a), int(score_b)]
-        return ''
+        return _data[9:]
+
+    @staticmethod
+    def is_date_between(date_str, start_date_str, end_date_str):
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+        return start_date <= date <= end_date
+
 
 # # Przykład użycia klasy
 # url_to_scrape = 'http://nalffutsal.pl/?page_id=34' # A
-# # url_to_scrape = 'http://nalffutsal.pl/?page_id=52' # B
-# scraper = MatchesScraper(url_to_scrape)
-# data_objects = scraper.scrape_matches()
-#
+url_to_scrape = 'http://nalffutsal.pl/?page_id=52' # B
+scraper = MatchesScraper(url_to_scrape)
+last_matches = scraper.scrape_matches('2024-01-15', '2024-01-18')
+print('------')
+print('poprzednia kolejka:')
+print('------')
+for match in last_matches['matches']:
+    for data in match:
+        print(f'{data}: {match[data]}')
+    print('------')
+
+future_matches = scraper.scrape_matches('2024-01-23', '2024-01-24')
+print('następna kolejka:')
+print('------')
+for match in future_matches['matches']:
+    for data in match:
+        print(f'{data}: {match[data]}')
+    print('------')
+
+
 # file_generator = JSONFileGenerator(data_objects)
 # file_generator.generate_and_save_file('matches-a.json')
 # # file_generator.generate_and_save_file('matches-b.json')
--- app/utils/nalf_table_scraper.py
+++ app/utils/nalf_table_scraper.py
@@ -12,7 +12,8 @@ class TableScraper(Scraper):
             data_object = {
                 'rank': row.find('td', class_='data-rank').text,
                 'name': row.find('a').text,
-                'team_logo': self._get_team_logo_name_from_link(row),
+                'logo_file': row.select_one('.data-name img')['src'],
+                'link': row.select_one('.data-name a')['href'],
                 'matches': int(row.find('td', class_='data-m').text),
                 'wins': int(row.find('td', class_='data-z').text),
                 'draws': int(row.find('td', class_='data-r').text),
@@ -24,13 +25,12 @@ class TableScraper(Scraper):
             data_objects_list.append(data_object)
         return data_objects_list
 
-    def _get_team_logo_name_from_link(self, link):
-        return link.select_one('.data-name img')['src'].split('/')[-1].split('.')[0]
-
-
+# TO DZIAŁA!!!
 # url_to_scrape = 'http://nalffutsal.pl/?page_id=16'
 # scraper = TableScraper(url_to_scrape)
 # data_objects = scraper.scrape_league_table()
 #
-# for o in data_objects:
-#     print(o['team_logo'])
+# for team in data_objects:
+#     for data in team:
+#         print(f'{data}: {team[data]}')
+#     print('------')
